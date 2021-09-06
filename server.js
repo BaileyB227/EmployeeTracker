e37d410643b23e09f43dda28188ed000f47bbf70
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-09-06 03:43:51 -0500

    added app.listen

--- server.js
+++ server.js
@@ -16,3 +16,11 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employees_db database.`)
 );
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+  
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
