977cefa5af9aea361cb4b4a1b3dae60fe0e81687
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-09-07 15:26:38 -0500

    view roles/depts/employees working

--- db/seed.sql
+++ db/seed.sql
@@ -5,4 +5,4 @@ INSERT INTO roles(title, salary, department_id)
 VALUES("Salesperson", 80000, 1), ("Lead Engineer", 150000, 2), ("Accountant", 125000, 3), ("Lawyer", 190000, 4);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ('John', 'Doe', 1, 12), ('Alex', 'Smith', 2, 34), ('Laura', 'Jones', 3, 26), ('Larry', 'Law', 4, null);
+VALUES ('John', 'Doe', 1, 1), ('Alex', 'Smith', 2, 2), ('Laura', 'Jones', 3, 3), ('Larry', 'Law', 4, null);
--- server.js
+++ server.js
@@ -10,28 +10,132 @@ app.use(express.json());
 
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'employees_db'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employees_DB'
     },
-    console.log(`Connected to the employees_db database.`)
+    console.log(`Connected to employees_db`)
 );
 
-app.get('/api/employee', (req, res) => {
-    const sql = 'SELECT * FROM employee'
+db.connect(function (err) {
+    if (err) throw err;
+    mainMenu();
+});
 
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-             return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
+function mainMenu() {
+    inquirer
+    .prompt({
+      type: "list",
+      name: "task",
+      message: "Would you like to do?",
+      choices: [
+        "View Employees",
+        "View Departments",
+        "View Roles",
+        "Add Department",
+        "Add Employee",
+        "Add Role",
+        "Update Employee Role",
+        "Quit"]
+    })
+    .then(function ({ task }) {
+      switch (task) {
+        case "View Employees":
+          viewEmployees();
+          break;
+
+        case "View Departments":
+          viewDepts();
+          break;
+      
+        case "View Roles":
+          viewRoles();
+          break;
+
+        case "Add Department":
+          addDept();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+        
+        case "Update Employee Role":
+          updateRole();
+          break;
+
+        case "Quit":
+          console.log("Bye!")
+          db.end();
+          break;
+      }
     });
-})
+}
+
+function viewEmployees() {
+  console.log("Viewing employees");
+
+  const sql = `SELECT employee.id, first_name, last_name, title, dname AS department, salary, manager_id
+  FROM employee
+  INNER JOIN roles
+  ON employee.role_id = roles.id
+  INNER JOIN department
+  ON roles.department_id = department.id`
+    
+  db.query(sql, (error, res) => {
+    if (error) throw error;
+    console.table(res);
+    mainMenu();
+    });
+}
+
+function viewDepts() {
+  console.log("Viewing Departments");
+
+  const sql = `SELECT id, dname AS department FROM department`
+
+  db.query(sql, (error, res) => {
+    if (error) throw error;
+    console.table(res);
+    mainMenu();
+  });
+}
+
+function viewRoles() {
+  console.log("Viewing Roles");
+
+  const sql  = `SELECT title, roles.id, dname AS department, salary
+  FROM department
+  JOIN roles ON department.id = roles.department_id`
+
+  db.query(sql, (error, res) => {
+    if (error) throw error;
+    console.table(res);
+    mainMenu();
+  });
+}
+
+function addDept() {
+
+
+}
+
+function updateRole(){
+
+
+}
+
+function addRole(){
+
+
+}
+
+
 
 app.use((req, res) => {
     res.status(404).end();
