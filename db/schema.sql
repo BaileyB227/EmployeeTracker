954576d315012f7d897a978a6374e4d82642659a
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-09-07 13:38:35 -0500

    fixed seed and schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,25 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     dname VARCHAR(30)
 );
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
 );
--- server.js
+++ server.js
@@ -18,6 +18,21 @@ const db = mysql.createConnection(
     console.log(`Connected to the employees_db database.`)
 );
 
+app.get('/api/employee', (req, res) => {
+    const sql = 'SELECT * FROM employee'
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+             return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+})
+
 app.use((req, res) => {
     res.status(404).end();
 });
